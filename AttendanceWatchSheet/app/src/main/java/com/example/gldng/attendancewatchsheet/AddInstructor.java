b39c5e57600edd832c1fcc002d2000fee43c9d8d
gldngullu <43879017+gldngullu@users.noreply.github.com>
2018-12-08 02:22:57 +0300

    Add User Interface Cont.

--- AttendanceWatchSheet/app/src/main/java/com/example/gldng/attendancewatchsheet/AddInstructor.java
+++ AttendanceWatchSheet/app/src/main/java/com/example/gldng/attendancewatchsheet/AddInstructor.java
@@ -6,14 +6,27 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class AddInstructor extends Fragment {
 
+    Spinner spinner;
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        Spinner spinner = (Spinner) getView().findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this.getContext(), R.array.Departments, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+    }
+
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater,ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.add_instructor_fragment,container,false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.add_instructor_fragment, container, false);
         return view;
 
     }
+
 }
--- AttendanceWatchSheet/app/src/main/java/com/example/gldng/attendancewatchsheet/AddUser.java
+++ AttendanceWatchSheet/app/src/main/java/com/example/gldng/attendancewatchsheet/AddUser.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.Toast;
 
 public class AddUser extends AppCompatActivity {
 
@@ -25,12 +26,40 @@ public class AddUser extends AppCompatActivity {
         addAdmin = findViewById(R.id.addAdmin);
         manager = getFragmentManager();
         frameLayout = findViewById(R.id.frameLayout);
+
+
+
+        addInstructor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addInstructor.setVisibility(View.INVISIBLE);
+                addAdmin.setVisibility(View.INVISIBLE);
+                addAssistant.setVisibility(View.INVISIBLE);
+                addInstructorFragmentAddition(v);
+            }
+        });
     }
+
+
 
     public void addInstructorFragmentAddition(View v){
         AddInstructor fragment1 = new AddInstructor();
         FragmentTransaction transaction = manager.beginTransaction();
         transaction.add(R.id.frameLayout,fragment1,"Fragment1");
         transaction.commit();
+    }
+
+    public void removeAddInstructorFragment(View v) {
+        addInstructor.setVisibility(View.VISIBLE);
+        addAdmin.setVisibility(View.VISIBLE);
+        addAssistant.setVisibility(View.VISIBLE);
+        AddInstructor fragment1 = (AddInstructor) manager.findFragmentByTag("Fragment1");
+        FragmentTransaction transaction = manager.beginTransaction();
+        if (fragment1 != null) {
+            transaction.remove(fragment1);
+            transaction.commit();
+        } else {
+            Toast.makeText(this,"There is no add instuctor fragment here", Toast.LENGTH_LONG).show();
+        }
     }
 }
