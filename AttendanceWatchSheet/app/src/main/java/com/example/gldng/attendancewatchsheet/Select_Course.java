117402d96a5befed2232f3822a0a7e945b384d3d
MertaliKoprl <37384188+MertaliKoprl@users.noreply.github.com>
2018-12-09 00:13:19 +0300

    Hadi Bismillah
    Basladik Darisi Diger Xmllere

---
+++ AttendanceWatchSheet/app/src/main/java/com/example/gldng/attendancewatchsheet/Select_Course.java
@@ -0,0 +1,13 @@
+package com.example.gldng.attendancewatchsheet;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Select_Course extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select__course);
+    }
+}
