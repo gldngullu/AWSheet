3513901b8c8dc8c7ec59b60d00aa8279c118a0de
Unknown <metinberkkaratas@gmail.com>
2018-12-09 22:40:21 +0300

    Attandance track selector page
    not finished yet

---
+++ AttendanceWatchSheet/app/src/main/java/com/example/gldng/attendancewatchsheet/AttandanceSelectorActivity.java
@@ -0,0 +1,14 @@
+package com.example.gldng.attendancewatchsheet;
+
+import android.os.Bundle;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AttandanceSelectorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_attandance_selector);
+    }
+}
