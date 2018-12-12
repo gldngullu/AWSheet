0c9e464cde3a17309be31da5f5d4651de3aa4caf
Unknown <metinberkkaratas@gmail.com>
2018-12-13 00:04:26 +0300

    QR code page xml completed.

--- AttendanceWatchSheet/app/src/main/java/com/example/gldng/attendancewatchsheet/AttandanceQrMethodActivity.java
+++ AttendanceWatchSheet/app/src/main/java/com/example/gldng/attendancewatchsheet/AttandanceQrMethodActivity.java
@@ -1,8 +1,16 @@
 package com.example.gldng.attendancewatchsheet;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
 
 public class AttandanceQrMethodActivity extends AppCompatActivity {
 
@@ -10,5 +18,18 @@ public class AttandanceQrMethodActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_attandance_qrmethod);
+
+        ImageView imageView = findViewById(R.id.qrLoc);
+        String text = "abc";
+        MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
+        try {
+            BitMatrix bitMatrix = multiFormatWriter.encode(text,BarcodeFormat.QR_CODE,200,200);
+            BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
+            Bitmap bitmap = barcodeEncoder.createBitmap(bitMatrix);
+            imageView.setImageBitmap(bitmap);
+        } catch (WriterException e) {
+            e.printStackTrace();
+        }
+
     }
 }
