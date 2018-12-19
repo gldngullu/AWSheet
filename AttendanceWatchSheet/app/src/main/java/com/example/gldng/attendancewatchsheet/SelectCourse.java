bc59e1121d2832295e28c3e978d1f864209ac459
MertaliKoprl <37384188+MertaliKoprl@users.noreply.github.com>
2018-12-20 02:04:42 +0300

    Template Navigation Barlar ve Headerlar Student-Instructor-Admin Xml'lerine eklendi - Hata veren Pngler yeniden assetlendi
    Darisi Back-Endin basina

---
+++ AttendanceWatchSheet/app/src/main/java/com/example/gldng/attendancewatchsheet/SelectCourse.java
@@ -0,0 +1,13 @@
+package com.example.gldng.attendancewatchsheet;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SelectCourse extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_course);
+    }
+}
