7f4eb30120080917f5958f9196a529b13778a695
korhankoz <43878790+korhankoz@users.noreply.github.com>
2018-12-21 19:12:56 +0300

    Succesful Register.

--- AttendanceWatchSheet/app/src/main/java/com/example/gldng/attendancewatchsheet/RegisterActivity.java
+++ AttendanceWatchSheet/app/src/main/java/com/example/gldng/attendancewatchsheet/RegisterActivity.java
@@ -5,10 +5,18 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class RegisterActivity extends AppCompatActivity {
     private EditText etMail;
@@ -45,15 +53,44 @@ public class RegisterActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent registerintent = new Intent(RegisterActivity.this,LoginActivity.class);
-
+                String mailinput=etMail.getText().toString();
+                String nameinput=etName.getText().toString();
+                String surnameinput=etSurname.getText().toString();
                 String passwordinput=etPassword.getText().toString();
                 String confirmpasswordinput=etConfirmPassword.getText().toString();
+                String secretqinput=etSecretQ.getText().toString();
                 if(0!=passwordinput.compareTo(confirmpasswordinput)){
                     Toast.makeText(RegisterActivity.this,"Confirmation Password is wrong",Toast.LENGTH_LONG).show();
                 }else {
-                    Toast.makeText(RegisterActivity.this, "Your registration request has been sent.", Toast.LENGTH_LONG).show();
+                    Response.Listener<String> responselistener = new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+
+                            try {
+                                JSONObject jsonResponse  = new JSONObject(response);
+                                int success = jsonResponse.getInt("success");
+                                if(success==1){
+                                    Log.d("successful","quary succesful");
+                                    Toast.makeText(RegisterActivity.this, "quary succesful", Toast.LENGTH_LONG).show();
+                                }else{
+                                    Log.d("unsuccessful","quary unsuccesful");
+                                    Toast.makeText(RegisterActivity.this, "quary unsuccesful", Toast.LENGTH_LONG).show();
+                                }
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+
+                        }
+                    };
+
+                    RegisterRequest registerRequest = new RegisterRequest(mailinput,nameinput,surnameinput,passwordinput,secretqinput,responselistener);
+                    RequestQueue queue = Volley.newRequestQueue(RegisterActivity.this);
+                    queue.add(registerRequest);
+
+                    //Toast.makeText(RegisterActivity.this, "Your registration request has been sent.", Toast.LENGTH_LONG).show();
                     RegisterActivity.this.startActivity(registerintent);
                     finish();
+
 
                 }
             }
---
+++ AttendanceWatchSheet/app/src/main/java/com/example/gldng/attendancewatchsheet/RegisterRequest.java
@@ -0,0 +1,27 @@
+package com.example.gldng.attendancewatchsheet;
+
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RegisterRequest extends StringRequest {
+    private static final String Register_request_url ="http://awsheet.cf/connect/register.php";
+private Map<String,String> params ;
+
+    public RegisterRequest (String mail, String name, String surname, String password, String secretq, Response.Listener<String> listener){
+     super(Method.POST,Register_request_url,listener,null);
+     params= new HashMap<>();
+        params.put("mailAdress",mail);
+     params.put("name",name);
+        params.put("surname",surname);
+        params.put("password",password);
+        params.put("secretq",secretq);
+    }
+
+    @Override
+    public Map<String, String> getParams() {
+        return params;
+    }
+}
